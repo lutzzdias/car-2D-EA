d1959d724aef7de364435fe2755d3bd00f14e23c
lutzzdias <thiagolutzdias@hotmail.com>
2024-05-10 17:05:31 +0100

    feat: implement swap mutation
    swaps two elements from the list that represent the same thing (wheel
    vertex, wheel size, edge size or edge angle)

--- Assets/EvolvingCars/TP2/Meta1/Mutation.cs
+++ Assets/EvolvingCars/TP2/Meta1/Mutation.cs
@@ -1,11 +1,14 @@
-using System.Diagnostics;
 using GeneticSharp.Domain.Chromosomes;
 using GeneticSharp.Domain.Mutations;
 using GeneticSharp.Runner.UnityApp.Car;
+using GeneticSharp.Domain.Randomizations;
+using UnityEngine;
+using System.Collections;
+using System;
 
 public class Mutation : IMutation
 {
-    public bool IsOrdered { get; private set; } // indicating whether the operator is ordered (if can keep the chromosome order).
+    public bool IsOrdered { get; private set; } // indicando se o operador é ordenado (se pode manter a ordem do cromossomo).
 
     public Mutation()
     {
@@ -14,12 +17,23 @@ public class Mutation : IMutation
 
     public void Mutate(IChromosome chromosome, float probability)
     {
-        
-        /* YOUR CODE HERE */
-        /*REPLACE THESE LINES BY YOUR MUTATION IMPLEMENTATION*/
-        CarChromosome newChromosome = new CarChromosome(((CarChromosome)chromosome).getConfig());
-        chromosome.ReplaceGenes(0, newChromosome.GetGenes());
-        /*END OF YOUR CODE*/
-    }
 
+        if (RandomizationProvider.Current.GetDouble() <= probability)
+        {
+            var castedChromosome = ((CarChromosome)chromosome);
+            var config = castedChromosome.getConfig();
+
+            int index1 = RandomizationProvider.Current.GetInt(1, castedChromosome.Length);
+
+            int multiplication = RandomizationProvider.Current.GetInt(1, 14);
+
+            int index2 = ((multiplication * 4) * index1) % castedChromosome.Length;
+
+            var gene1 = castedChromosome.GetGene(index1);
+            var gene2 = castedChromosome.GetGene(index2);
+
+            castedChromosome.ReplaceGene(index1, gene2);
+            castedChromosome.ReplaceGene(index2, gene1);
+        }
+    }
 }
